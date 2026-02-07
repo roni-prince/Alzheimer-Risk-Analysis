f93913093d490c221cd3af18c7da6bf5ff72f578
hannarvsh <hannarv27@gmail.com>
2026-02-07 16:47:45 +0200

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -5,6 +5,7 @@ from scipy import stats
 import logging
 
 logger = logging.getLogger(__name__)
+#allows log massages can be traced to their source.(Because there are a few files)
 
 # Visual Settings
 PALETTE = ["#b30047", "#ff4dc4", "#0000e6", "#9999ff"]
@@ -17,7 +18,10 @@ def analyze_lifestyle(df: pd.DataFrame):
     """
     # Statistical Calculation
     contingency = pd.crosstab(df['Smoking_bin'], df['Diagnosis_bin'])
-    chi2, p_val, _, _ = stats.chi2_contingency(contingency)
+    _, p_val, _, _ = stats.chi2_contingency(contingency)
+    #diagnosis_bin- after the cleaning, we are left with a binary bin,
+    #allows easy work with the data.
+    #'_'- means that this value is there and exists, yet it is not in use by choice.
     
     # Visualization
     plt.figure(figsize=(8, 5))
@@ -30,6 +34,7 @@ def analyze_lifestyle(df: pd.DataFrame):
     plt.show()
     
     logger.info(f"Lifestyle analysis complete. P-value: {p_val:.4f}")
+    #f- float, .4- four numbers after the dot.
 
 def analyze_physical_activity(df: pd.DataFrame):
     """
@@ -44,19 +49,20 @@ def analyze_physical_activity(df: pd.DataFrame):
     group_healthy = df[df['Diagnosis_bin'] == 0]['PhysicalActivity']
     group_alzheimer = df[df['Diagnosis_bin'] == 1]['PhysicalActivity']
     
-    t_stat, p_val = stats.ttest_ind(group_healthy, group_alzheimer, nan_policy='omit')
+    _, p_val = stats.ttest_ind(group_healthy, group_alzheimer, nan_policy='omit')
+    #nan_policy= what to do when there are missing (NaN) values in the data.
     
     # Visualization
     plt.figure(figsize=(8, 5))
-    ax = sns.pointplot(
+    sns.pointplot(
         data=df, 
         x='Diagnosis_bin', 
         y='PhysicalActivity', 
-        hue='Diagnosis_bin',
+        hue='Diagnosis_bin',#addes legend (מקרא)
         palette=[PALETTE[2], PALETTE[0]], # Blue for Healthy (2), Red for Alzheimer (0)
-        join=False,       
+        join=False,#don't connect the two catagories, in order to be able to compare them.
         capsize=.1,
-        scale=1.2
+        scale=1.2#enlarges the dots size.
     )
     
     status = "Significant" if p_val < SIGNIFICANCE_LEVEL else "Not Significant"
@@ -77,7 +83,7 @@ def analyze_education(df: pd.DataFrame):
     """
     # Group data by Education level for ANOVA
     edu_groups = [df[df['EducationLevel'] == i]['MMSE'] for i in sorted(df['EducationLevel'].unique())]
-    f_stat, p_val = stats.f_oneway(*edu_groups)
+    _, p_val = stats.f_oneway(*edu_groups)
     
     # Visualization
     plt.figure(figsize=(8, 5))
@@ -97,7 +103,7 @@ def analyze_sleep(df: pd.DataFrame):
     """
     group_no = df[df['MemoryComplaints_bin'] == 0]['SleepQuality']
     group_yes = df[df['MemoryComplaints_bin'] == 1]['SleepQuality']
-    t_stat, p_val = stats.ttest_ind(group_no, group_yes)
+    _, p_val = stats.ttest_ind(group_no, group_yes)
     
     # Visualization
     plt.figure(figsize=(8, 5))
